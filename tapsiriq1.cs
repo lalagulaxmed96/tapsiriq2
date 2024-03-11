2cc638d0e564ef7859e2905d855e2564fbfa652b
lalagulaxmed96 <tu6g4w77k@code.edu.az>
2024-03-11 11:37:31 +0400

    commit

---
+++ tapsiriq1.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Eded daxil edin ");
+
+int eded = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+int hasil = 1;
+
+while (eded > 0)
+{
+    a = eded % 10;
+    hasil *= a;
+    eded /= 10;
+}
+
+Console.WriteLine(hasil);
---
+++ tapsiriq2.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Eded daxil edin ");
+
+int eded = Convert.ToInt32(Console.ReadLine());
+
+int faktorial = 1;
+int addim = 1;
+
+while (eded <= addim)
+{
+   
+        factorial *= addim;
+	addim++;
+        
+}
+
+Console.WriteLine(faktorial);
+
+
+
+
+
